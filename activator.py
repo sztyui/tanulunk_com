57f23c0722420539f7487b047830a799d28fa5a1
Szilágyi István <i10815@eon.com>
2023-05-19 11:34:29 +0200

    play video and wait for the end

--- activator.py
+++ activator.py
@@ -10,14 +10,15 @@ from config.load import open_config
 class TanulunkFiller:
     """Helps to full tanulunk.com and runs coupons"""
 
-    def __init__(self, config):
+    def __init__(self, config, delay: int = 2):
         self.config = config
+        self.delay = delay
 
-    def _click_button(self, name: str) -> bool:
+    def _click_button(self, name: str, confidence: bool = 0.8) -> bool:
         if name not in self.config["images"]:
             raise AttributeError(f"no button: {name}")
         button = pyautogui.locateCenterOnScreen(
-            self.config["images"][name], confidence=0.8
+            self.config["images"][name], confidence=confidence
         )
         if not button:
             logging.error(f"button not found: {name}")
@@ -30,13 +31,43 @@ class TanulunkFiller:
         return self._click_button("coupon")
 
     def watch_advertisment(self):
+        """Click on watch advertisment button"""
         return self._click_button("advertisment")
+
+    def play_video(self) -> bool:
+        """Click on play video button"""
+        return self._click_button(name="play", confidence=0.9)
+
+    def wait_until_vide_ends(self):
+        """Search for video end screen"""
+        while True:
+            ends = pyautogui.locateCenterOnScreen(
+                self.config["images"]["video_end"], confidence=0.8
+            )
+            if ends:
+                return
+            time.sleep(self.delay)
+
+    def click_on_back_button(self) -> bool:
+        """Click on back button upon video finish."""
+        return self._click_button("back")
 
     def run(self):
         """Main runner, acts like facade."""
-        self.click_coupon()
-        time.sleep(1)
-        self.watch_advertisment()
+        while True:
+            if not self.click_coupon():
+                return
+            time.sleep(self.delay)
+            if not self.watch_advertisment():
+                return
+            time.sleep(self.delay)
+            if not self.play_video():
+                return
+            time.sleep(self.delay)
+            self.wait_until_vide_ends()
+            if not self.click_on_back_button():
+                return
+            time.sleep(self.delay)
 
 
 if __name__ == "__main__":
